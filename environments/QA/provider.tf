66f3d6121e3ed010c33f59df01c53481c986b456
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-09 11:16:06 -0400

    Update provider.tf

--- environments/QA/provider.tf
+++ environments/QA/provider.tf
@@ -1,18 +1,26 @@
+###############################################################################
+# versions.tf – QA Environment                                               #
+###############################################################################
+
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
+      version = "~> 3.100"  # stick to current major for stability
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
+      version = "~> 2.22"   # matches AKS 1.29+ API versions
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.12.0" # ✅ Add this
+      version = "~> 2.12"
     }
   }
+}
 
-  required_version = ">= 1.3.0"
+provider "azurerm" {
+  features {}
 }
