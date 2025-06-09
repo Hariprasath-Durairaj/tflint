4fe6233a88dec70b0dcb4f401c762c69b3b5fc52
Hariprasath-Durairaj <Hariprasath.Durairaj@dtconsultinggroup.ca>
2025-06-09 10:19:15 -0400

    Update outputs.tf

--- terraform-modules/terraform-azure-tag-policy/outputs.tf
+++ terraform-modules/terraform-azure-tag-policy/outputs.tf
@@ -1,7 +1,9 @@
 output "policy_definition_id" {
-  value = azurerm_policy_definition.require_tags.id
+  description = "Resource ID of the Enterprise-Tag Policy Definition"
+  value       = azurerm_policy_definition.require_enterprise_tags.id
 }
 
 output "policy_assignment_id" {
-  value = azurerm_policy_assignment.require_tags.id
+  description = "Resource ID of the Enterprise-Tag Policy Assignment"
+  value       = azurerm_policy_assignment.require_enterprise_tags.id
 }
